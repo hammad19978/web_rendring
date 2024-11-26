0093af243e5614be0e93253af39bce0a0a1c6f77
HAMMAD KHAN <82611112+hammad19978@users.noreply.github.com>
2024-11-26 15:37:36 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ def greet_user(name):
 
     if name:
         print("Name is provided.")
-    print("End of function.")  # This should be indented to match the if block
+        print("End of function.")  # This should be indented to match the if block
 
 greet_user("Alice")
